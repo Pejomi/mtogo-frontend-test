bf1b709cbafd319449b8a22f71585689d6f83512
MieThorberg <cph-mt373@cphbusiness.dk>
2023-11-30 17:58:02 +0100

    setup packages and cucumber

---
+++ src/test/java/dk/pejomi/mtogofrontendtest/bdd/CucumberConfiguration.java
@@ -0,0 +1,9 @@
+package dk.pejomi.mtogofrontendtest.bdd;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+
+@CucumberContextConfiguration
+@ContextConfiguration
+public class CucumberConfiguration {
+}
