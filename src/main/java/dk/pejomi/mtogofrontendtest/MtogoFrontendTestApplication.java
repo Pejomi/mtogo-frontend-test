a9a6a1edeb73e5053df149abc11a38d8bb6d5679
MieThorberg <cph-mt373@cphbusiness.dk>
2023-11-30 17:06:38 +0100

    setup project

---
+++ src/main/java/dk/pejomi/mtogofrontendtest/MtogoFrontendTestApplication.java
@@ -0,0 +1,13 @@
+package dk.pejomi.mtogofrontendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtogoFrontendTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtogoFrontendTestApplication.class, args);
+    }
+
+}
