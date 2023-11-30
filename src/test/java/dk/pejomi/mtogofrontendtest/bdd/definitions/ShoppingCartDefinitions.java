e2617dd92faf550a11a6ae3c48890314312df9b6
MieThorberg <cph-mt373@cphbusiness.dk>
2023-11-30 18:07:27 +0100

    added definitions for feature

---
+++ src/test/java/dk/pejomi/mtogofrontendtest/bdd/definitions/ShoppingCartDefinitions.java
@@ -0,0 +1,21 @@
+package dk.pejomi.mtogofrontendtest.bdd.definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ShoppingCartDefinitions {
+    @Given("the customer is viewing a product")
+    public void theCustomerIsViewingAProduct() {
+
+    }
+
+    @When("the customer clicks on the {string} button")
+    public void theCustomerClicksOnTheButton(String arg0) {
+
+    }
+
+    @Then("the item should be added to the shopping cart")
+    public void theItemShouldBeAddedToTheShoppingCart() {
+    }
+}
